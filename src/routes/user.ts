df26bd3ae7ffcab7b3234fc09fbd255bf50f06bb
iinow <iinow@naver.com>
2020-09-13 19:26:56 +0900

    init commit

---
+++ src/routes/user.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { apiVersion } from '~/common/decorator'
+
+let router = express.Router()
+
+router.get('/', (req, res) => {
+  res.json({
+    name: 'dhfkdhfkdhfk'
+  })
+})
+
+export default router
